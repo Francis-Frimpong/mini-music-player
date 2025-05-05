9ba9e17834c5424c010a07d9f79b4ca0fff4011e
Francis <brakay1136@gmail.com>
2025-05-05 06:55:00 -0700

    fixed the next track function

---
+++ app.js
@@ -0,0 +1,35 @@
+const trackTitleAndArtist = document.querySelector('.artist-name-title');
+const nextSong = document.querySelector('.fa-forward-step');
+
+
+
+
+
+
+
+const playList = [
+    "alone-296348.mp3",
+    "dont-talk-315229.mp3",
+    "epic-trailer-background-music-emotional-dramatic-cinematic-intro-249656.mp3",
+    "experimental-cinematic-hip-hop-315904.mp3",
+    "gardens-stylish-chill-303261.mp3",
+    "happy-playful-music-cute-sweet-kid-child-baby-background-intro-theme-277965.mp3",
+    "hip-hop-black-angel-mid-2-161266.mp3",
+    "in-slow-motion-inspiring-ambient-lounge-219592.mp3",
+    "kugelsicher-by-tremoxbeatz-302838.mp3",
+    "so-fresh-315255.mp3",
+    "spring-easter-day-music-30-seconds-version-320430.mp3"
+];
+
+let currentSong = 0;
+trackTitleAndArtist.textContent = playList[currentSong];
+
+function nextTrack() {
+    trackTitleAndArtist.textContent = playList[++currentSong] 
+    if(currentSong >= playList.length) {
+        currentSong = 0;
+        trackTitleAndArtist.textContent = playList[currentSong];
+    }   
+}
+
+nextSong.addEventListener('click', nextTrack)
